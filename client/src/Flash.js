3d7cd01012d3cfb4a1eb39269776b391cb69761f
fpazaran <138263417+fpazaran@users.noreply.github.com>
2024-02-05 18:28:04 -0800

    Flash in src

--- client/src/Flash.js
+++ client/src/Flash.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import SomeComponent from './src/SomeComponent';
+import SomeComponent from './SomeComponent';
 
 export default function App() {
   return (
