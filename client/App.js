4177571759a4ae115a7fa89997d625f364cfcd3d
aaronli16 <liaaron53@gmail.com>
2024-02-05 18:12:35 -0800

    Test

--- client/App.js
+++ client/App.js
@@ -5,7 +5,7 @@ import SomeComponent from './src/SomeComponent';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>okokokokokko</Text>
+      <Text>test</Text>
       <StatusBar style="auto" />
       <SomeComponent />
     </View>
