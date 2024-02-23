23901f8cfa985aea97eb9b3ed741af1ee650a7e1
aaronli16 <liaaron53@gmail.com>
2024-02-23 14:33:02 -0800

    vugv

---
+++ client/assets/StyleSheet.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+export const styles = StyleSheet.create({
+    container1: {
+        color: 'B7A57A', // gold
+        alignItems: 'center',
+        height: 509,
+        padding: 30, // not actual padding, placeholder
+        marginHorizontal: 44,
+    },
+    container2: {
+        color: 'FFFFFF', // white
+        alignItems: 'center',
+        height: 40,
+        padding: 30,
+        marginHorizontal: 68,
+    },
+    registerContainer: {
+        color: '4B2E83', // purple
+        alignItems: 'center',
+        height: 40.78,
+        padding: 30,
+        marginHorizontal: 114,
+    }
+  });
+    
