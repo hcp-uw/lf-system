3bc5c392cb13bf73df511ad00f0013d531821b56
fpazaran <138263417+fpazaran@users.noreply.github.com>
2024-02-23 13:54:09 -0800

    App.js

--- client/src/screens/Registration.js
+++ client/src/screens/Registration.js
@@ -1,21 +1,9 @@
-import { Text, StyleSheet } from "react-native"; // Here we are using the React Library
+import { StyleSheet, Text, View } from 'react-native';
 
-// By using export, you can import and use this component in your app!
-export default SomeComponent = () => {
-  let number = 100;
-
-  // This is what we want the component to return.
+export default function Registration() {
   return (
-    <Text style={styles.text}> {/* Here I am using the styles StyleSheet to change the text's color. */}
-      This is a component.
-      I can display data, {number}, using curly brackets!
-    </Text>
-  )
+    <View style={styles.container}>
+      <Text>testicles</Text>
+    </View>
+  );
 }
-
-// You can style components and tags using StyleSheet
-const styles = StyleSheet.create({
-  text: {
-    color: 'red',
-  },
-});
