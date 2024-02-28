ed6ea1bd82ea6ef2e13bc8ce902b4bed4cb91c5b
vera chuang <verachuanghh0809@gmail.com>
2024-02-28 14:38:27 -0800

    added landingpage to app screen

--- client/App.js
+++ client/App.js
@@ -1,14 +1,16 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Registration from './src/screens/Registration';
+import LandingPage from './src/screens/LandingPage';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName = "Registration"> 
+      <Stack.Navigator initialRouteName = "LandingPage"> 
         <Stack.Screen name='Registration' component={Registration} />
+        <Stack.Screen name='LandingPage' component={LandingPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
