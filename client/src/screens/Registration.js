842dbbb91002c4c707eabd0e1cb59edb4aa6808f
aaronli16 <liaaron53@gmail.com>
2024-03-01 14:37:08 -0800

    button

--- client/src/screens/Registration.js
+++ client/src/screens/Registration.js
@@ -1,5 +1,6 @@
 import { Text, View } from 'react-native';
 import { styles } from '../assets/StyleSheet';
+import {PurpleButton, button} from '../components/button';
 
 export default function Registration({navigation}){
   return (
@@ -14,9 +15,11 @@ export default function Registration({navigation}){
         </View>
         <View style={styles.container2}>
         </View>
-        <View style={styles.loginContainer}>
+        <View style={styles.container2}>
         </View>
-
+        <View style={styles.container2}>
+        </View>
+        <PurpleButton navigation = {navigation} navigationPage = 'LandingPage'/>
       </View>
 
     </View>
