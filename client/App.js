73ab5c3f06d3a0b10c573ed45833cefa44318a8d
Shravani Bhujbal <shravanibh21@gmail.com>
2024-02-29 16:13:34 -0800

    Styled page headers
    Update in App.js, all page headers will be purple background and white text.
    Added login page to the Stack navigator.

--- client/App.js
+++ client/App.js
@@ -1,15 +1,27 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Registration from './src/screens/Registration';
+import LandingPage from './src/screens/LandingPage'
+import Login from './src/screens/Login';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName = "Registration"> 
+      <Stack.Navigator initialRouteName = "Registration" screenOptions={{
+        headerStyle: {
+          backgroundColor: '#39275B', // Set header background color
+        },
+        headerTintColor: '#fff', // Set text color of header title and buttons
+        headerTitleStyle: {
+          fontWeight: 'bold', // Set font weight of header title
+        },
+      }}> 
         <Stack.Screen name='Registration' component={Registration} />
         <Stack.Screen name='LandingPage' component={LandingPage} />
+        <Stack.Screen name='Login' component={Login} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
