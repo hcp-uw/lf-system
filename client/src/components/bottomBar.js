52d0e1fc2dd06ed2d52a614c49a559f9324748de
Shravani Bhujbal <shravanibh21@gmail.com>
2024-04-01 17:26:08 -0700

    Updates to stylesheet

--- client/src/components/bottomBar.js
+++
@@ -1,8 +0,0 @@
-import { View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
-import { styles } from "../assets/StyleSheet";
-
-export const bottomBar = ({navigation, navigationPage})  => {
-    const onIconPress = () => navigation.navigate(navigationPage);
-
-    
-}
