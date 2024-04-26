a00245cbd7d421067b6f254fdf332771ff98156f
aaronli16 <liaaron53@gmail.com>
2024-04-26 13:23:15 -0700

    camera updates

--- client/src/screens/profile.js
+++ client/src/screens/profile.js
@@ -1,68 +1,55 @@
-import { Text, View, TouchableOpacity, Image } from 'react-native'
-import React from 'react';
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
 import { styles } from '../assets/StyleSheet';
-import {PostItemButton, PurpleButton} from '../components/button';
-import { InputContainer } from '../components/inputContainer';
 import IonIcon from "react-native-vector-icons/Ionicons";
-//import UserPermission from '../utilities/UserPermssions'
-//import * as ImagePicker from 'expo-image-picker'
-// import * as firebase from 'firebase';
+import { useNavigation } from '@react-navigation/native';
 
-export default function Profile({navigation}){
-    state = {
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       user: {
         name: "",
         email: "",
         password: "",
         avatar: null
       },
-
     };
-    // handlePickAvatar = aysnc () => {
-    //   UserPermission.getCameraPermission();
+  }
 
-    //   let result = await ImagePicker.launchImageLibraryAysnc({
-    //     mediaTypes: ImagePicker.MediaTypeOptions.Images,
-    //     allowsEditing: true,
-    //     aspect: [4,3]
+  handlePickAvatar = () => {
+    this.props.navigation.navigate('Camera', {
+      setAvatarUrl: (newAvatarUrl) => {
+        this.setState({ user: { ...this.state.user, avatar: newAvatarUrl } });
+      }
+    });
+  }
+  
 
-    //   });
-    //   if (result.cancelled){
-    //     this.setState({user: {... this.state.user, avatar: result.uri }});
-    //   };
-    // };
+  render() {
     return (
-        <View 
-        screenOptions={{
-          
-        }}
-        style={{
+      <View style={{
         justifyContent: 'center',
         alignContent: 'center', 
         flex : 1,
         gap: 20,
-        }}>
-          
-          
-            <TouchableOpacity style = {styles.avatarPlaceHolder} /*onPress={this.handlePickAvatar}*/ >
-              {/* <Image source={{uri: this.state.user.avatar}} style = {styles.avatar}/> */}
-              <IonIcon name="person-circle-outline" size={100} />
-            </TouchableOpacity>
-         
-        
-          <View style={[styles.goldBigContainer, { height: 131, } ]}>
-            <InputContainer placeholderText = 'Name'/>
-            <InputContainer placeholderText = 'Campus'/> 
-          </View>
+      }}>
+        <TouchableOpacity style={styles.avatarPlaceHolder} onPress={this.handlePickAvatar}>
+          {this.state.user.avatar ? (
+            <Image source={{ uri: this.state.user.avatar }} style={styles.avatar} />
+          ) : (
+            <IonIcon name="person-circle-outline" size={100} />
+          )}
+        </TouchableOpacity>
 
-          <View style = {[styles.scrollView, {height: '40%', }]}>
-            <Text style = {styles.postText}>Posts</Text>
-            {/*make this go to the post implement later*/}
-            <PostItemButton navigation = {navigation} navigationPage = 'Login' buttonText='Login'/> 
-        
-          </View>
-  
+        {/* Rest of your component */}
       </View>
-       );
-       
-     }
+    );
+  }
+}
+
+export default function(props) {
+    const navigation = useNavigation();
+
+    return <Profile {...props} navigation={navigation} />;
+}
