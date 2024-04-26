66dcf693db294959c8e0831d0ca91962b924897d
aaronli16 <liaaron53@gmail.com>
2024-04-26 13:41:29 -0700

    profile updates

--- client/src/screens/profile.js
+++ client/src/screens/profile.js
@@ -1,55 +1,47 @@
-import React, { Component } from 'react';
+
+import React, { useState } from 'react';
 import { Text, View, TouchableOpacity, Image } from 'react-native';
 import { styles } from '../assets/StyleSheet';
 import IonIcon from "react-native-vector-icons/Ionicons";
 import { useNavigation } from '@react-navigation/native';
 
-class Profile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {
-        name: "",
-        email: "",
-        password: "",
-        avatar: null
-      },
-    };
-  }
 
-  handlePickAvatar = () => {
-    this.props.navigation.navigate('Camera', {
+
+
+export default function Registration({ navigation }) {
+  const navigation = useNavigation();
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    avatar: null
+  });
+
+  const handlePickAvatar = () => {
+    navigation.navigate('Camera', {
       setAvatarUrl: (newAvatarUrl) => {
-        this.setState({ user: { ...this.state.user, avatar: newAvatarUrl } });
+        setUser({...user, avatar: newAvatarUrl});
       }
     });
   }
-  
 
-  render() {
-    return (
-      <View style={{
-        justifyContent: 'center',
-        alignContent: 'center', 
-        flex : 1,
-        gap: 20,
-      }}>
-        <TouchableOpacity style={styles.avatarPlaceHolder} onPress={this.handlePickAvatar}>
-          {this.state.user.avatar ? (
-            <Image source={{ uri: this.state.user.avatar }} style={styles.avatar} />
-          ) : (
-            <IonIcon name="person-circle-outline" size={100} />
-          )}
-        </TouchableOpacity>
+  return (
+    <View style={{
+      justifyContent: 'center',
+      alignContent: 'center', 
+      flex : 1,
+      gap: 20,
+    }}>
+      <TouchableOpacity style={styles.avatarPlaceHolder} onPress={handlePickAvatar}>
+        {user.avatar ? (
+          <Image source={{ uri: user.avatar }} style={styles.avatar} />
+        ) : (
+          <IonIcon name="person-circle-outline" size={100} />
+        )}
+      </TouchableOpacity>
 
-        {/* Rest of your component */}
-      </View>
-    );
-  }
+      {/* You can continue adding more UI elements here as per your original component */}
+    </View>
+  );
 }
 
-export default function(props) {
-    const navigation = useNavigation();
-
-    return <Profile {...props} navigation={navigation} />;
-}
